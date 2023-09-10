bb3430aef355f82cccdefa6a2a98a78687a5e25d
MiracleTest <revellingStoic@mail.ru>
2023-09-10 16:56:12 +0300

    Update for next resume

--- src/hooks/useLanguage.tsx
+++ src/hooks/useLanguage.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react"
 import { LanguageContext } from "../providers/languageProvider"
 
+
 const useLanguage = () => {
     const value = useContext(LanguageContext)
     return value
